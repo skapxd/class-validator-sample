80d16f14a1667d068d23c182b81a1e8c727a55f7
Manuel <hbiaser132@gmail.com>
2023-12-22 10:22:16 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { plainToClass } from "class-transformer";
+import { IsBoolean, IsNumber, IsString, validate } from "class-validator";
+
+const jsonData = {
+  "id": "1", // -> should fail validate, because it's not a type number
+  "title": "Upgrade typescript runtime validation with class decorators",
+  "done": false,
+}
+
+class Todo {
+  @IsNumber()
+  id!: number;
+
+  @IsString()
+  title!: string;
+
+  @IsBoolean()
+  done!: boolean;
+}
+
+const convertedClass = plainToClass(Todo, jsonData);
+
+validate(convertedClass)
+.then((errors) => console.log('then: ',errors))
+
+
+
